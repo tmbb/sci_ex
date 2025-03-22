d2737268a0d2280b73e46381c9fc7755ce3c2e23
Tiago Barroso <tmbb@campus.ul.pt>
2025-03-22 01:49:19 +0000

    docs

--- lib/sci_ex/operators.ex
+++ lib/sci_ex/operators.ex
@@ -1,4 +1,11 @@
 defmodule SciEx.Operators do
+  @moduledoc """
+  Operators to use with arrays.
+
+  To use operators in your module you just have to `use SciEx.Operators`.
+  The operators imported by this module also work with normal Elixir numbers
+  (floating point numbers or integers).
+  """
   defmacro __using__(_opts) do
     quote do
       import Kernel, except: [+: 2, -: 2, *: 2, /: 2], warn: false
